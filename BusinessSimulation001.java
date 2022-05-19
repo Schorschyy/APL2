e5566a039c30bd5f1e38860720f57a3b3fac56ea
Schorschyy <96734352+Schorschyy@users.noreply.github.com>
2022-05-20 00:29:04 +0200

    Add files via upload

---
+++ BusinessSimulation001.java
@@ -0,0 +1,94 @@
+package demo001.copy;
+
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class BusinessSimulation001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int spielen = 1;
+		int taler = 0;
+		
+		System.out.println("Wirtschaftssimulation Programmierung II");
+		Niederlassung niederlassungEinbeck = new Niederlassung("Einbeck", Warenart.BIER, 1);
+		Lager lagerAugsburg = new Lager("Augsburg");
+			
+			do{
+			niederlassungEinbeck.anfordern();
+			niederlassungEinbeck.produzieren();
+			int produzierteMengeEinbeck = niederlassungEinbeck.abholen();
+			Warenart produzierteWarenart = niederlassungEinbeck.getWarenart();
+	
+			System.out.println("Ort: "+niederlassungEinbeck.getOrt()+"\tWare: "
+					+niederlassungEinbeck.getWarenartString()+"\tMenge: "+produzierteMengeEinbeck);
+				
+			lagerAugsburg.einlagern(produzierteWarenart, produzierteMengeEinbeck);
+	
+			System.out.println("Aktueller Bestand im Lager:");
+			for (Iterator<Warenart> warenartIterator = lagerAugsburg.getEingelagerteWaren().iterator(); warenartIterator.hasNext();) {
+				Warenart warenart = (Warenart) warenartIterator.next();
+				System.out.println(Util.convertWarenartToString(warenart)+": "+lagerAugsburg.getBestand(warenart));
+			}
+			
+			
+			int verkaufsmenge;
+			Scanner menge_scanner = new Scanner(System.in);
+			
+			do {
+				System.out.println("Wieviel verkaufen?");
+				verkaufsmenge = menge_scanner.nextInt();
+			}while((verkaufsmenge<0)||(verkaufsmenge>lagerAugsburg.getBestand(niederlassungEinbeck.getWarenart())));
+			
+			
+			int preis = 10;
+			
+			int erloes = lagerAugsburg.verkaufen(produzierteWarenart,verkaufsmenge , preis);
+			
+			taler = taler + erloes;
+			
+			
+			System.out.println("Erlös durch Verkauf: "+erloes);
+			System.out.println("Aktueller Bestand im Lager:");
+			
+			for (Iterator<Warenart> warenartIterator = lagerAugsburg.getEingelagerteWaren().iterator(); warenartIterator.hasNext();) {
+				Warenart warenart = (Warenart) warenartIterator.next();
+				System.out.println(Util.convertWarenartToString(warenart)+": "+lagerAugsburg.getBestand(warenart));
+			}
+		
+			System.out.println("Taler gesamt: " +taler );
+			
+			String weiterspielen;
+			Scanner spielen_scanner = new Scanner(System.in);
+			do {
+				System.out.println("Nächste Runde?");
+				weiterspielen = spielen_scanner.next();
+				
+				if( weiterspielen.equals ("j")) {
+					spielen=1;
+					System.out.println("====================================");
+				}
+				
+				if( weiterspielen.equals ("n")) {
+					spielen=0;
+					System.out.println("====================================");
+					System.out.println("=============Game=Over==============");
+					System.out.println("====================================");
+				
+				}
+				
+			}while(!(weiterspielen.equals("j")) && !(weiterspielen.equals ("n")));
+			
+		}while(spielen==1);
+		
+		
+		
+		
+		
+		
+	}
+
+	
+
+}
---
+++ Firmenstandort.java
@@ -0,0 +1,16 @@
+package demo001.copy;
+
+public class Firmenstandort {
+
+
+public Firmenstandort(String ort) {
+	this.ort = ort;
+}
+	
+public String ort; 
+
+public String getOrt() {
+	return ort;
+}
+	 
+}
